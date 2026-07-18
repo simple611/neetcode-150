5ecdda7feeae19a97f3bbb84fd460a6f6cbc0de5
Simple Swaroop <45093435+simple611@users.noreply.github.com>
2026-07-18 18:25:23 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+
+        if(strs.empty()) return "";
+        vector<int> sizes;
+
+        for(auto& s: strs){
+            sizes.push_back(s.size());
+        }
+        string res;
+        for(int sz: sizes){
+            res.append(to_string(sz));
+            res.push_back(',');
+        }
+        res.push_back('#');
+        for(auto& s: strs){
+            res.append(s);
+        }
+
+        return res;
+        
+    }
+
+    vector<string> decode(string s) {
+        if(s.empty()) return {};
+
+        vector<int> sizes;
+
+        vector<string> res;
+
+        int i=0;
+        while(s[i] != '#'){
+            int j=i;
+            while(s[j] != ','){
+                j++;
+            }
+
+            sizes.push_back(stoi(s.substr(i,j-i)));
+            i=j+1;
+        }
+        i++;
+        for(int sz: sizes){
+            res.push_back(s.substr(i,sz));
+            i=i+sz;
+        }
+
+        return res;
+
+    }
+};
